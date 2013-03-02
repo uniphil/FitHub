7115661190df8e880f1afd4c452ce4cc3d10c642
Clarence Leung <clarle@yahoo-inc.com>
2013-03-02 18:23:09 -0500

    Counting jumping jacks (prototype)

--- kinect/Kinect.Client/Socket.js
+++ kinect/Kinect.Client/Socket.js
@@ -4,6 +4,7 @@
     var context = canvas.getContext("2d");
 
     var skeleton = new Skeleton(status, canvas, context);
+    var exercise = new Exercise(skeleton);
 
     if (!window.WebSocket) {
         status.innerHTML = "Your browser does not support web sockets!";
@@ -35,10 +36,15 @@
         // Render the skeleton(s)
         skeleton.render(jsonObject);
 
+        // Check for repetition completion
+        exercise.updateRepetitions(jsonObject);
+
         // Output the angles
+        /*
         var leftAngle = skeleton.jointAngle(jsonObject, 'handleft', 'shouldercenter', 'hipcenter')[0];
         var rightAngle = skeleton.jointAngle(jsonObject, 'handright', 'shouldercenter', 'hipcenter')[0];
         console.log('left : ' + leftAngle + " | right : " + rightAngle);
+        */
 
         // Inform the server about the update.
         socket.send("Skeleton updated on: " + (new Date()).toDateString() + ", " + (new Date()).toTimeString());
