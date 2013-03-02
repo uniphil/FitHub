f229c66dce5dc2c489198c01ab36bbd6f44291c3
Clarence Leung <clarle@yahoo-inc.com>
2013-03-02 12:48:49 -0500

    Working coordination of jumping jack tracking.

--- kinect/Kinect.Client/web-sockets.js
+++ kinect/Kinect.Client/web-sockets.js
@@ -34,10 +34,18 @@
         context.fillStyle = "#FF0000";
         context.beginPath();
 
+        var repCompleted = false;
+
         // Display the skeleton joints.
         for (var i = 0; i < jsonObject.skeletons.length; i++) {
             for (var j = 0; j < jsonObject.skeletons[i].joints.length; j++) {
                 var joint = jsonObject.skeletons[i].joints[j];
+
+                if (joint.name === "handleft" || joint.name === "handright") {
+                    if (joint.y < 20) {
+                        repCompleted = true;
+                    }
+                }
 
                 // Draw!!!
                 context.arc(parseFloat(joint.x), parseFloat(joint.y), 10, 0, Math.PI * 2, true);
@@ -51,9 +59,20 @@
             draw(jsonObject.skeletons[i].joints);
         }
 
+        if (repCompleted) {
+            throttledUpdate();
+        }
+
         // Inform the server about the update.
         socket.send("Skeleton updated on: " + (new Date()).toDateString() + ", " + (new Date()).toTimeString());
     };
+
+    var throttledUpdate = _.debounce(function () {
+            var repetitions = $("#repetitions"),
+                numberReps = parseInt(repetitions.html());
+
+            repetitions.html(numberReps + 1);
+        }, 100);
 
     var drawLimb = function (data, start, end) {
         var from, to;
