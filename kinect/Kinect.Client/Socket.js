25e5a35ab4e3cac6b54044a993d4b3b19c5ed9ac
Clarence Leung <clarle@yahoo-inc.com>
2013-03-02 15:56:41 -0500

    More modular client-side code

---
+++ kinect/Kinect.Client/Socket.js
@@ -0,0 +1,41 @@
+﻿window.onload = function () {
+    var status = document.getElementById("status");
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    var skeleton = new Skeleton(status, canvas, context);
+
+    if (!window.WebSocket) {
+        status.innerHTML = "Your browser does not support web sockets!";
+        return;
+    }
+
+    status.innerHTML = "Connecting to server...";
+
+    // Initialize a new web socket.
+    var socket = new WebSocket("ws://localhost:8181/KinectHtml5");
+
+    // Connection established.
+    socket.onopen = function () {
+        status.innerHTML = "Connection successful.";
+    };
+
+    // Connection closed.
+    socket.onclose = function () {
+        status.innerHTML = "Connection closed.";
+    }
+
+    // Receive data FROM the server!
+    socket.onmessage = function (evt) {
+        status.innerHTML = "Kinect data received.";
+
+        // Get the data in JSON format.
+        var jsonObject = eval('(' + evt.data + ')');
+
+        // Render the skeleton(s)
+        skeleton.render(jsonObject);
+        
+        // Inform the server about the update.
+        socket.send("Skeleton updated on: " + (new Date()).toDateString() + ", " + (new Date()).toTimeString());
+    };
+};
