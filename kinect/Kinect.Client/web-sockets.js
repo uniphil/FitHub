e6db5dbd01372bc65f5dbf02de0a790977e72009
Clarence Leung <clarle@yahoo-inc.com>
2013-03-02 14:44:33 -0500

    Re-serializing to redo jumping jacks

--- kinect/Kinect.Client/web-sockets.js
+++ kinect/Kinect.Client/web-sockets.js
@@ -30,24 +30,18 @@
         // Get the data in JSON format.
         var jsonObject = eval('(' + evt.data + ')');
 
+        jsonObject = convertJsonData(jsonObject);
+
         context.clearRect(0, 0, canvas.width, canvas.height);
         context.fillStyle = "#FF0000";
         context.beginPath();
 
-        var repCompleted = false;
+        /* var repCompleted = false; */
 
         // Display the skeleton joints.
         for (var i = 0; i < jsonObject.skeletons.length; i++) {
-            for (var j = 0; j < jsonObject.skeletons[i].joints.length; j++) {
-                var joint = jsonObject.skeletons[i].joints[j];
-
-                if (joint.name === "handleft" || joint.name === "handright") {
-                    if (joint.y < 20) {
-                        repCompleted = true;
-                    }
-                }
-
-                // Draw!!!
+            for (var jointName in jsonObject.skeletons[i].joints) {
+                var joint = jsonObject.skeletons[i].joints[jointName];
                 context.arc(parseFloat(joint.x), parseFloat(joint.y), 10, 0, Math.PI * 2, true);
             }
         }
@@ -59,13 +53,17 @@
             draw(jsonObject.skeletons[i].joints);
         }
 
+        /*
         if (repCompleted) {
             throttledUpdate();
         }
+        */
 
         // Inform the server about the update.
         socket.send("Skeleton updated on: " + (new Date()).toDateString() + ", " + (new Date()).toTimeString());
     };
+
+    /*
 
     var throttledUpdate = _.debounce(function () {
             var repetitions = $("#repetitions"),
@@ -74,21 +72,10 @@
             repetitions.html(numberReps + 1);
         }, 100);
 
-    var drawLimb = function (data, start, end) {
-        var from, to;
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].name === start) {
-                from = data[i];
-                break;
-            }
-        }
+    */
 
-        for (var j = 0; j < data.length; j++) {
-            if (data[j].name === end) {
-                to = data[j];
-                break;
-            }
-        }
+    var drawLimb = function (data, start, end) {
+        var from = data[start], to = data[end];
 
         if (from && to) {
             context.strokeStyle = '#fff'; // red
@@ -121,4 +108,21 @@
         drawLimb(data, "kneeright", "ankleright");
         drawLimb(data, "ankleright", "footright");
     };
+
+    var convertJsonData = function (data) {
+        joints = {}
+        for (var i = 0; i < data.skeletons.length; i++) {
+            for (var j = 0; j < data.skeletons[i].joints.length; j++) {
+                var joint = data.skeletons[i].joints[j];
+                joints[joint.name] = {
+                    x: joint.x,
+                    y: joint.y,
+                    z: joint.z
+                }
+            }
+            data.skeletons[i].joints = joints;
+        }
+
+        return data;
+    }
 };
