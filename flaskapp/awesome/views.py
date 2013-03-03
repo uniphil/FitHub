41a9a75cb240db658447fc4cc346bcaf09a8b658
Chris Wu <me@chriswu.me>
2013-03-03 06:27:09 -0500

    foobar

--- flaskapp/awesome/views.py
+++ flaskapp/awesome/views.py
@@ -111,6 +111,7 @@ def recieve_message():
 
 @app.route('/demo')
 def demo():
+  import ipdb; ipdb.set_trace()
   return render_template('demo.html', live=request.args.get('live', 'false') == 'true')
 
 @app.errorhandler(404)
